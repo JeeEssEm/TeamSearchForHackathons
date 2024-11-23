53e3425231baa2002246497041c85142145de9dc
ilyaKiselev <kisel.ds@gmail.com>
2024-11-23 17:21:28 +0300

    added all fields in user model

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 from typing import TYPE_CHECKING
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String, Text
+from sqlalchemy import Integer, String, Boolean, DateTime, func
 from database import AbstractBase
+from datetime import datetime
 
 if TYPE_CHECKING:
     from .role import Role
@@ -17,10 +18,13 @@ class User(AbstractBase):
     uni: Mapped[str] = mapped_column(String(50), nullable=True)
     year_of_study: Mapped[int] = mapped_column(Integer, nullable=True)
     group: Mapped[str] = mapped_column(String(50), nullable=True)
-    about_me: Mapped[str] = mapped_column(Text, nullable=True)
-    achievements: Mapped[str] = mapped_column(Text, nullable=True)
+    about_me: Mapped[str] = mapped_column(String(300), nullable=True)
     resume: Mapped[str] = mapped_column(String(255), nullable=True)
     avatar: Mapped[str] = mapped_column(String(255), nullable=True)
+    moderator_id: Mapped[int] = mapped_column(Integer)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+    form_status: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_form_private: Mapped[bool]= mapped_column(Boolean,default=False)
 
     roles: Mapped[list["Role"]] = relationship(
         "Role", secondary="users_roles", back_populates="users"
